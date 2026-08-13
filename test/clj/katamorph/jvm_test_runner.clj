c3f5c37048966e9682d13c4af0d150c220a061e4
Err <foamy125@gmail.com>
2026-08-13 18:02:46 -0500

    test(workflow): cover typed composition laws

--- test/clj/katamorph/jvm_test_runner.clj
+++ test/clj/katamorph/jvm_test_runner.clj
@@ -1,10 +1,16 @@
 (ns katamorph.jvm-test-runner
   (:require [clojure.test :as test]
+            [katamorph.schema.action-semantics-test]
             [katamorph.schema.registry-test]
-            [katamorph.schema.validation-test]))
+            [katamorph.schema.step-dataflow-test]
+            [katamorph.schema.validation-test]
+            [katamorph.workflow.validation-test]))
 
 (defn -main [& _]
   (let [result (test/run-tests 'katamorph.schema.registry-test
-                               'katamorph.schema.validation-test)]
+                               'katamorph.schema.validation-test
+                               'katamorph.schema.action-semantics-test
+                               'katamorph.schema.step-dataflow-test
+                               'katamorph.workflow.validation-test)]
     (when (pos? (+ (:fail result) (:error result)))
       (throw (ex-info "Katamorph JVM tests failed" result)))))
