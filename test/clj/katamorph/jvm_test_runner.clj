708f1bb6ecc6221cd8d9bbd0ba7559537bdd46a6
Err <foamy125@gmail.com>
2026-08-13 17:48:39 -0500

    test(condition): include portable condition suites

--- test/clj/katamorph/jvm_test_runner.clj
+++ test/clj/katamorph/jvm_test_runner.clj
@@ -1,10 +1,14 @@
 (ns katamorph.jvm-test-runner
   (:require [clojure.test :as test]
+            [katamorph.condition-test]
+            [katamorph.condition.path-test]
             [katamorph.schema.registry-test]
             [katamorph.schema.validation-test]))
 
 (defn -main [& _]
   (let [result (test/run-tests 'katamorph.schema.registry-test
-                               'katamorph.schema.validation-test)]
+                               'katamorph.schema.validation-test
+                               'katamorph.condition.path-test
+                               'katamorph.condition-test)]
     (when (pos? (+ (:fail result) (:error result)))
       (throw (ex-info "Katamorph JVM tests failed" result)))))
