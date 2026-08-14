8a22a1b6f3537bcfd82c8b22450a799253ccd0e4
Err <foamy125@gmail.com>
2026-08-14 06:37:38 -0500

    test(action): run invocation application suite

--- test/clj/katamorph/jvm_test_runner.clj
+++ test/clj/katamorph/jvm_test_runner.clj
@@ -1,5 +1,6 @@
 (ns katamorph.jvm-test-runner
   (:require [clojure.test :as test]
+            [katamorph.action.application-test]
             [katamorph.action.invocation-test]
             [katamorph.condition-test]
             [katamorph.condition.path-test]
@@ -10,7 +11,8 @@
             [katamorph.schema.validation-test]))
 
 (defn -main [& _]
-  (let [result (test/run-tests 'katamorph.action.invocation-test
+  (let [result (test/run-tests 'katamorph.action.application-test
+                               'katamorph.action.invocation-test
                                'katamorph.condition-test
                                'katamorph.condition.path-test
                                'katamorph.schema.action-semantics-test
